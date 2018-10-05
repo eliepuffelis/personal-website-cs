bbbdea1f9188ab0739876c26e745602ee685cae5
Elie Puffelis <eliepuffelis@gmail.com>
2018-10-05 13:35:43 -0700

    navbar, banner, about section, menu functionality

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Elie Puffelis</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="menu">
+            <div id="menuClose" onclick="closeMenu()">
+            <div id="topOptions"></div>
+            <div id="bottomOptions"></div>
+
+            </div></div>
+        <div id="parrallax"></div>
+        <nav id="navbar">
+            <div id="emblem"><a href="index.html"></a></div>
+            <div id="menubars" onclick="openMenu()">
+                <div class="bars"></div>
+                <div class="bars"></div>
+                <div class="bars"></div>
+            </div>
+        </nav>
+        <div id="banner"><div>Elie Puffelis</div></div>
+        <div id="about">
+        <div id="aboutContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam labore nulla distinctio veritatis vero praesentium soluta maxime. Vitae, earum. Veniam doloribus unde obcaecati praesentium necessitatibus vel tenetur quam numquam excepturi pariatur! Omnis id, aspernatur hic dolorum explicabo accusantium dolor, delectus similique, nam voluptas qui sed officia recusandae deserunt in quas quis voluptatem autem culpa voluptate debitis saepe repellat odit molestias. Quam, eveniet neque. Commodi voluptatibus cum cumque quibusdam veniam amet, nihil aspernatur blanditiis, reprehenderit quam temporibus deleniti rerum libero iusto nesciunt beatae consectetur ad velit laboriosam dignissimos. Cupiditate magni labore quas alias ducimus blanditiis, at quis, illo, iste quasi dignissimos.</div>
+        </div>
+        <div id="section"></div>
+        
+        
+
+
+
+
+
+
+    </div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    var menu = document.getElementById("menu");
+    menu.style.opacity = '1';
+    menu.style.visibility = 'visible';
+}
+function closeMenu(){
+    var menu = document.getElementById("menu");
+    menu.style.opacity = '0';
+    menu.style.visibility = 'hidden';
+}
