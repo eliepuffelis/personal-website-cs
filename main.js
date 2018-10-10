5c5880b1576c874076641477529437cf2ecb62f1
Elie Puffelis <eliepuffelis@gmail.com>
2018-10-09 18:05:49 -0700

    start website with welcome screen, made menu, and started about me

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
+window.onload = welcome;
+
+function welcome(){
+    setInterval(function(){ 
+
+        var welcome = document.getElementById("welcome");
+        welcome.style.opacity = '0';
+        welcome.style.visibility = 'hidden';
+        showName();
+
+     }, 2000);
+    
+}
+
+function showName(){
+    setInterval(function(){ 
+
+        var name = document.getElementById("name");
+        name.style.opacity = '1';
+        name.style.visibility = 'visible';
+        }, 1100);
+}
+
+
 function openMenu(){
     var menu = document.getElementById("menu");
-    menu.style.opacity = '1';
     menu.style.visibility = 'visible';
+    menu.style.left = '0';
 }
 function closeMenu(){
     var menu = document.getElementById("menu");
-    menu.style.opacity = '0';
     menu.style.visibility = 'hidden';
+    menu.style.left = '-150%';
 }
